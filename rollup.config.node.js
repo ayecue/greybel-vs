b9ec3841bb5c88373d7d0174f4f354802f37bcce
ayecue <soerenwehmeier@googlemail.com>
2023-06-14 20:54:05 +0200

    1.6.5 optimize build output

--- rollup.config.node.js
+++ rollup.config.node.js
@@ -1,6 +1,8 @@
 const commonjs = require('@rollup/plugin-commonjs');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const terser = require('@rollup/plugin-terser').default;
 const json = require('@rollup/plugin-json');
+const nodePolyfills = require('rollup-plugin-polyfill-node');
 const dotenv = require('rollup-plugin-dotenv').default;
 
 const options = {
@@ -9,16 +11,27 @@ const options = {
         file: 'extension.js',
         name: 'greyscript',
         exports: 'named',
-        format: 'cjs'
+        format: 'cjs',
+        globals: {
+            'vscode': 'vscode',
+            'path': 'path',
+            'axios': 'require(\'axios\')'
+        }
     },
     plugins: [
         dotenv(),
         json(),
         commonjs({
+            esmExternals: ['vscode', 'path'],
             sourceMap: false
         }),
+        nodePolyfills(),
+        nodeResolve({
+            preferBuiltins: false
+        }),
         terser()
-    ]
+    ],
+    external: ['vscode', 'path', 'axios']
 };
 
 export default options;
