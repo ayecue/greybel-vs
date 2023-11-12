374bcc81f9a51c8caae3513ab913e356f548e4e2
ayecue <soerenwehmeier@googlemail.com>
2023-11-12 14:59:20 +0100

    2.0.0 switch operators

--- src/autocomplete/operators.ts
+++ src/autocomplete/operators.ts
@@ -1,4 +1,5 @@
 import { CompletionItem } from '@vscode/debugadapter';
+import { Operator as GreybelOperators } from 'greybel-core/dist/types/operators';
 import { Operator } from 'miniscript-core';
 import { CompletionItemKind } from 'vscode';
 
@@ -19,11 +20,11 @@ export const AVAILABLE_OPERATORS: CompletionItem[] = [
   Operator.SubtractShorthand,
   Operator.MultiplyShorthand,
   Operator.DivideShorthand,
-  Operator.BitwiseAnd,
-  Operator.BitwiseOr,
-  Operator.LeftShift,
-  Operator.RightShift,
-  Operator.UnsignedRightShift,
+  GreybelOperators.BitwiseAnd,
+  GreybelOperators.BitwiseOr,
+  GreybelOperators.LeftShift,
+  GreybelOperators.RightShift,
+  GreybelOperators.UnsignedRightShift,
   Operator.Assign,
   Operator.Reference
 ].map((item: string) => new CompletionItem(item, CompletionItemKind.Operator));
