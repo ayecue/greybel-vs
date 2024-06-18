fc6a79260ffb0c895b2539d29f58e6c943f56d74
ayecue <soerenwehmeier@googlemail.com>
2024-06-18 13:00:58 +0200

    2.3.7 update packages

--- rollup.config.api.js
+++ rollup.config.api.js
@@ -4,49 +4,49 @@ const { babel } = require('@rollup/plugin-babel');
 const terser = require('@rollup/plugin-terser').default;
 const json = require('@rollup/plugin-json');
 const nodePolyfills = require('rollup-plugin-polyfill-node');
-const externalGlobals  = require('rollup-plugin-external-globals');
+const externalGlobals = require('rollup-plugin-external-globals');
 const dotenv = require('rollup-plugin-dotenv').default;
 
 const options = {
-    input: 'out/api/view.js',
-    output: {
-        file: 'api.view.js',
-        format: 'iife'
-    },
-    plugins: [
-        dotenv(),
-        externalGlobals({
-            'react': 'React',
-            'react-dom': 'ReactDOM',
-            'prismjs': 'Prism',
-            'react-markdown': 'ReactMarkdown'
-        }),
-        json(),
-        commonjs(),
-        nodePolyfills(),
-        nodeResolve({
-            browser: true,
-            preferBuiltins: false
-        }),
-        babel({
-            presets: ['@babel/preset-react', '@babel/preset-env', {
-                exclude: "transform-typeof-symbol"
-            }],
-            babelHelpers: 'runtime',
-            plugins: [
-                ["@babel/plugin-transform-runtime", {
-                    regenerator: true
-                }]
-            ]
-        }),
-        terser()
-    ],
-    external: [
-        'react',
-        'react-dom',
-        'prismjs',
-        'react-markdown'
-    ]
+  input: 'out/api/view.js',
+  output: {
+    file: 'api.view.js',
+    format: 'iife'
+  },
+  plugins: [
+    dotenv(),
+    externalGlobals({
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      'prismjs': 'Prism',
+      'react-markdown': 'ReactMarkdown'
+    }),
+    json(),
+    commonjs(),
+    nodePolyfills(),
+    nodeResolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    babel({
+      presets: ['@babel/preset-react', '@babel/preset-env', {
+        exclude: "transform-typeof-symbol"
+      }],
+      babelHelpers: 'runtime',
+      plugins: [
+        ["@babel/plugin-transform-runtime", {
+          regenerator: true
+        }]
+      ]
+    }),
+    terser()
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'prismjs',
+    'react-markdown'
+  ]
 };
 
 export default options;
--- rollup.config.node.js
+++ rollup.config.node.js
@@ -5,51 +5,51 @@ const json = require('@rollup/plugin-json');
 const dotenv = require('rollup-plugin-dotenv').default;
 
 const options = {
-    input: 'out/extension.js',
-    output: {
-        file: 'extension.js',
-        name: 'greyscript',
-        exports: 'named',
-        format: 'cjs',
-        globals: {
-            'vscode': 'vscode'
-        }
-    },
-    plugins: [
-        dotenv(),
-        json(),
-        commonjs({
-            sourceMap: false,
-            ignoreDynamicRequires: ['net']
-        }),
-        nodeResolve({
-            preferBuiltins: false
-        }),
-        terser()
-    ],
-    external: [
-        'vscode',
-        "path",
-        "fs",
-        "url",
-        "net",
-        "events",
-        "os",
-        "https",
-        "stream",
-        "http",
-        "util",
-        "tty",
-        "readline",
-        "crypto",
-        "zlib",
-        "tls",
-        "dns",
-        "querystring",
-        "steam-user",
-        "node-json-stream",
-        "greybel-agent"
-    ]
+  input: 'out/extension.js',
+  output: {
+    file: 'extension.js',
+    name: 'greyscript',
+    exports: 'named',
+    format: 'cjs',
+    globals: {
+      'vscode': 'vscode'
+    }
+  },
+  plugins: [
+    dotenv(),
+    json(),
+    commonjs({
+      sourceMap: false,
+      ignoreDynamicRequires: ['net']
+    }),
+    nodeResolve({
+      preferBuiltins: false
+    }),
+    //terser()
+  ],
+  external: [
+    'vscode',
+    "path",
+    "fs",
+    "url",
+    "net",
+    "events",
+    "os",
+    "https",
+    "stream",
+    "http",
+    "util",
+    "tty",
+    "readline",
+    "crypto",
+    "zlib",
+    "tls",
+    "dns",
+    "querystring",
+    "steam-user",
+    "node-json-stream",
+    "greybel-agent"
+  ]
 };
 
 export default options;
