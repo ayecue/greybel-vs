07702d479c605605a30385e6c161465b1fae4824
ayecue <soerenwehmeier@googlemail.com>
2024-08-17 10:43:26 +0200

    2.4.9 update language server

--- src/language-client-web.ts
+++ src/language-client-web.ts
@@ -3,14 +3,13 @@ import { workspace, ExtensionContext, Uri } from 'vscode';
 import {
   LanguageClient,
   LanguageClientOptions
-} from 'vscode-languageclient/browser';
+} from 'vscode-languageclient/lib/browser/main';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-  const serverMain = Uri.joinPath(context.extensionUri, 'node_modules/greybel-languageserver/browser.js');
+export async function activate(context: ExtensionContext) {
+  const serverMain = Uri.joinPath(context.extensionUri, 'node_modules/greybel-languageserver-browser/index.js');
   const worker = new Worker(serverMain.toString(true));
-
   const clientOptions: LanguageClientOptions = {
     documentSelector: [{ scheme: 'file', language: 'greyscript' }],
     synchronize: {
@@ -27,12 +26,17 @@ export function activate(context: ExtensionContext) {
   );
 
   client.registerProposedFeatures();
-  client.start();
+
+  try {
+    await client.start();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export function deactivate(): Thenable<void> | undefined {
+export async function deactivate(): Promise<void> {
   if (!client) {
-    return undefined;
+    return;
   }
-  return client.stop();
+  return await client.stop();
 }
